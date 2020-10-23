76d57e13b45b9513503cffa1fbd583e88fed4f33
Myroslav <havrushkomyroslav@gmail.com>
2020-10-23 13:05:08 +0300

    Done-Lesson001_Task6

---
+++ Lesson001_Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson001_Task6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            bool name = false;
+            Console.WriteLine($"IsMarried: {name}");
+        }
+    }
+}
